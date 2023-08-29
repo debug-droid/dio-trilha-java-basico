51a15a6c6609d21cd535f62ff0d7e8b500a445d8
Diego <DiegoprogBr@hotmail.com>
2023-08-29 18:28:26 -0300

    Desafio finalizado e o README também

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -3,20 +3,23 @@ import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) {
         
-        Scanner scanner = new Scanner(System.in);
+        try (Scanner scanner = new Scanner(System.in)) {
 
-        System.out.println("Por favor, digite o seu nome completo: ");
-        String NomeCliente = scanner.nextLine();
+            System.out.println("Por favor, digite o seu nome completo: ");
+            String NomeCliente = scanner.nextLine();
 
-        System.out.println("Por favor, digite o número da sua agência: ");
-        String agencia = scanner.nextLine();
+            System.out.println("Por favor, digite o número da sua agência: ");
+            String agencia = scanner.nextLine();
 
-        System.out.println("Por favor, digite o número da sua conta: ");
-        int conta = scanner.nextInt();
+            System.out.println("Por favor, digite o número da sua conta: ");
+            int conta = scanner.nextInt();
 
-        System.out.println("Por favor, digite o seu saldo: ");
-        double saldo = scanner.nextDouble();
+            System.out.println("Por favor, digite o seu saldo: ");
+            double saldo = scanner.nextDouble();
 
-        System.out.println("Olá " + NomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque.");
+            System.out.println("Olá ".concat(NomeCliente) + ", obrigado por criar uma conta em nosso banco, sua agência é ".concat(agencia) + ", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque.");
+        } catch (Exception e) {
+            System.out.println("Aconteceu algo inesperado.");
+          }
     }
 }
